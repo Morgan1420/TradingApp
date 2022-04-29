abc4dec9d23078af84811b32733efe5a93e24864
Morgan <76940280+Morgan1420@users.noreply.github.com>
2022-04-29 23:31:50 +0200

    Add files via upload

---
+++ main/java/com/example/tradingsimulator/MyStockList.kt
@@ -0,0 +1,11 @@
+package com.example.tradingsimulator
+
+class MyStockList{
+    companion object {
+        val stockList = ArrayList<StockModel>()
+        init {
+            stockList.add(StockModel("Apple", 0.0, 0.0))
+            stockList.add(StockModel("Amazon", 0.0, 0.0))
+        }
+    }
+}
---
+++ main/java/com/example/tradingsimulator/StockModel.kt
@@ -0,0 +1,3 @@
+package com.example.tradingsimulator
+
+data class StockModel(var name: String, var currentValue: Double, var changeValue:Double)
